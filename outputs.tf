baf957f26c1f0990c9ae5331afbdb123c68ab7e2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "function_app_active_slots_id" {
+  description = "Map of id values across all function_app_active_slots, keyed the same as var.function_app_active_slots"
+  value       = { for k, v in azurerm_function_app_active_slot.function_app_active_slots : k => v.id }
+}
 output "function_app_active_slots_last_successful_swap" {
   description = "Map of last_successful_swap values across all function_app_active_slots, keyed the same as var.function_app_active_slots"
   value       = { for k, v in azurerm_function_app_active_slot.function_app_active_slots : k => v.last_successful_swap }
